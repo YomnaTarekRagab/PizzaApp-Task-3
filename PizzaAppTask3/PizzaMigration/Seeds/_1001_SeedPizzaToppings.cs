6c7540233d40bcc1ce5da4e649e2b8f31265cebc
manaarr <manaarrzakaria@gmail.com>
2021-08-19 17:22:14 +0200

    Seed Tables

---
+++ PizzaAppTask3/PizzaMigration/Seeds/_1001_SeedPizzaToppings.cs
@@ -0,0 +1,36 @@
+﻿using FluentMigrator;
+
+namespace PizzaMigration.Seeds
+{
+    public class PizzaToppings
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+
+    [Migration(1001)]
+    public class _1001_SeedPizzaToppings : FluentMigrator.Migration
+    {
+        public static List<PizzaToppings> PizzaTopping = new()
+        {
+            new() { Name = "Chicken BBQ", Price = 15 },
+            new() { Name = "Shrimps", Price = 30 },
+            new() { Name = "Pepperoni", Price = 12.5 },
+            new() { Name = "Vegetables", Price = 8.4 },
+            new() { Name = "Meat", Price = 17.5 },
+        };
+
+        public override void Up()
+        {
+            foreach(var topping in PizzaTopping)
+            {
+                Insert.IntoTable(Tables.PizzaToppings).Row(
+                    new
+                    {
+                        Toppings = topping.Name,
+                        Price = topping.Price
+                    });
+            }
+        }
+    }
+}
---
+++ PizzaAppTask3/PizzaMigration/Seeds/_1003_SeedPizzaSides.cs
@@ -0,0 +1,36 @@
+﻿using FluentMigrator;
+
+namespace PizzaMigration.Seeds
+{
+    public class PizzaSides
+    {
+        public string Type { get; set; }
+        public double Price { get; set; }
+    }
+
+    [Migration(3)]
+    public class _1003_SeedPizzaSides : FluentMigrator.Migration
+    {
+        public static List<PizzaSides> PizzaSide = new()
+        {
+            new() { Type = "Fries", Price = 5.5 },
+            new() { Type = "None", Price = 0.0 },
+            new() { Type = "Cookie", Price = 3.5 },
+            new() { Type = "Salad", Price = 9.8 },
+            new() { Type = "Garlic Bread", Price = 7.7 },
+        };
+
+        public override void Up()
+        {
+            foreach (var side in PizzaSide)
+            {
+                Insert.IntoTable(Tables.PizzaSides).Row(
+                    new
+                    {
+                        Sides = side.Type,
+                        Price = side.Price
+                    });
+            }
+        }
+    }
+}
